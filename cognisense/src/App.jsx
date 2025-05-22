c4f614c0e3dddee7a480e6dacc501fa2b851f9bf
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2025-05-22 12:42:11 -0400

    edited the html file, created a button, and fixed layout

--- cognisense/src/App.jsx
+++ cognisense/src/App.jsx
@@ -11,7 +11,20 @@ function App() {
         <a href="https://example.com"  target="_blank">
           <img src={telephone} className="logo telephone" alt="Telephone" />
         </a>
+        <div className="intro">
+        <p className="intro-paragraph">Cognisense is an AI powered law platform that specializes in quick, efficient, and accurate completion
+          of simple divorces. We will guide through each step of the way, so you don't have to feel overwhelmed the 
+          paperwork. Utilizing AI, you will feel your questions and concerns will be heard and answered. Plus,
+          you will not have to pay exuberant fees to complete your simple divorce.
+        </p>
+         {/* btn */}
+         <div className="btn-1">
+        <a href="#">Find out more</a>
       </div>
+      </div>
+       
+      </div>
+     
       <Footer />
     </>
   )
