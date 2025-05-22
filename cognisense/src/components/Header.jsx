cbdad51000a354b1a97d2b7f931c588a9d0f725d
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2025-05-22 17:49:33 -0400

    added nav-bar and changed the hero image

--- cognisense/src/components/Header.jsx
+++ cognisense/src/components/Header.jsx
@@ -2,6 +2,16 @@ export default function Header() {
     return (
         <header>
             <h1>Cognisense</h1>
+
+          <div className="nav-bar">
+            <ul class="nav-items">
+                <li><a href="">Home</a></li>
+                <li><a href="">Sign-Up</a></li>
+                <li><a href="">Sign-In</a></li>
+                <li><a href="">AI Chat</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
             
         </header>
     )
